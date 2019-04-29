66b281b45d4eccd84685d93a1140ced1748735cc
peterjewicz <peterjewicz@totalwebconnections.com>
2019-04-29 07:49:56 -0500

    small cleanup

--- resources/public/cljs-out/dev/cal_test/core.js
+++
@@ -1,1 +0,0 @@
-// Compiled by ClojureScript 1.10.339 {:static-fns true, :optimize-constants true}
--- resources/public/cljs-out/dev/figwheel/main/generated/dev_auto_test_runner.cljs
+++
@@ -1,5 +0,0 @@
-(ns figwheel.main.generated.dev-auto-test-runner
-  (:require [cljs.test :refer-macros [run-tests]]
-            [cljs-test-display.core] [cal-test.core-test]))
-
-  (run-tests (cljs-test-display.core/init! "app-auto-testing") (quote cal-test.core-test))
